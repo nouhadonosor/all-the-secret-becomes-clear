af9b986caab9947bf6f408ce40b075226d924531
nouhadonosor <nouhadonosor@gmail.com>
2024-06-07 13:35:30 +0300

    extensions

---
+++ cpp_file.cpp
@@ -0,0 +1,15 @@
+
+void main()
+{
+    char login = 'admin';
+    char password = 'xxxxxxxx';
+    char login = 'root';
+    char password = 'qwerty123';
+
+    char api_key = '12345678901234567890123456789012';
+    char api_token = '12345678901234567890123456789012';
+
+    char token = 'fe9d2881-6180-4e7c-979c-8f36e1225eaa';
+
+    char github = 'ghp_zBdnihx0k8sp9maZNBjtbjf3N12fFy1gtcXa';
+}
---
+++ js_file.js
@@ -0,0 +1,11 @@
+let login = 'admin';
+let password = 'xxxxxxxx';
+login = 'root';
+password = 'qwerty123';
+
+let api_key = '12345678901234567890123456789012';
+let api_token = '12345678901234567890123456789012';
+
+let token = 'fe9d2881-6180-4e7c-979c-8f36e1225eaa';
+
+let github = 'ghp_zBdnihx0k8sp9maZNBjtbjf3N12fFy1gtcXa';
