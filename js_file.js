9238e2fc9a6138beccf0a264ef4c108f9f9320fa
nouhadonosor <51091282+nouhadonosor@users.noreply.github.com>
2024-08-29 16:50:58 +0300

    Update js_file.js

--- js_file.js
+++ js_file.js
@@ -6,6 +6,4 @@ password = 'qwerty123';
 let api_key = '12345678901234567890123456789012';
 let api_token = '12345678901234567890123456789012';
 
-let token = 'fe9d2881-6180-4e7c-979c-8f36e1225eaa';
-
 let github = 'ghp_zBdnihx0k8sp9maZNBjtbjf3N12fFy1gtcXa';
